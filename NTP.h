0664d45fb95bb5b0d54548ba389e753d92338562
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-05-20 12:59:49 +0530

    initial file commit

---
+++ NTP.h
@@ -0,0 +1,16 @@
+/****************************************************************
+* NTP LIBRARY
+*
+* MAY 20 2017
+*
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ------------
+****************************************************************/
+
+#ifndef _NTP_H_
+#define _NTP_H_
+
+#endif
